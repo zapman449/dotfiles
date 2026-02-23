e0fa29c0b177459a85ae0319b1cb9c5881f0c0b8
Jason Price <jprice@squareup.com>
2026-02-23 10:43:37 -0500

    use glow pagination for markdown files

--- setup.sh
+++ setup.sh
@@ -38,4 +38,6 @@ if [[ "${UNAME}" == "Darwin" ]]; then
     stow_ghostty_dir="${HOME}/Library/Application Support/com.mitchellh.ghostty"
     [[ ! -d "${stow_ghostty_dir}" ]] && mkdir -p "${stow_ghostty_dir}"
     stow ghostty --target="${stow_ghostty_dir}"
+
+    stow glow "--target=${HOME}/Library/Preferences/glow"
 fi
